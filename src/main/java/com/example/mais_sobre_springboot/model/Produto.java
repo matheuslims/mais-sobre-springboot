951c90a79f3f07acc62274715e70144ac039d600
matheuslims <omatheusdasilvalima@gmail.com>
2025-03-21 11:58:30 -0300

    Implementando a classe Modelo de Produto para o banco

---
+++ src/main/java/com/example/mais_sobre_springboot/model/Produto.java
@@ -0,0 +1,39 @@
+package com.example.mais_sobre_springboot.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private Double preco;
+
+    public Produto() {}
+
+    public Produto(String nome, Double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+}
