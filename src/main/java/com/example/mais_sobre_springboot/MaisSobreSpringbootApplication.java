efa5639287b578b311b1df5eac0a9bf558d8dcf8
matheuslims <omatheusdasilvalima@gmail.com>
2025-03-21 11:19:15 -0300

    first commit

---
+++ src/main/java/com/example/mais_sobre_springboot/MaisSobreSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mais_sobre_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisSobreSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisSobreSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mais_sobre_springboot/MaisSobreSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mais_sobre_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaisSobreSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
