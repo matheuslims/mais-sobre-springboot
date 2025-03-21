9f0eb185d933589f031225a26217c38362a73676
matheuslims <omatheusdasilvalima@gmail.com>
2025-03-21 11:31:13 -0300

    Subindo a aplicação na web

---
+++ src/main/java/com/example/mais_sobre_springboot/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.mais_sobre_springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Olá mundo! Bem-vindo ao SpringBoot";
+    }
+
+}
